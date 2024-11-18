92b869a2fab743e3d618e83f3ab32e9adaa90d58
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2024-11-18 22:20:58 +0545

    Question5 completed

--- lib/q5_sum_natural_num.dart
+++ lib/q5_sum_natural_num.dart
@@ -1,2 +1,11 @@
 // Write a dart program to calculate the sum of natural numbers.
 
+int sumOfNaturalNumbers(int n) {
+  return n * (n + 1) ~/ 2;
+}
+
+void main() {
+  int n = 10;
+  print("Sum of $n natural numbers: ${sumOfNaturalNumbers(n)}");
+}
+
